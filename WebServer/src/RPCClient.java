afb5825447d0bcfd25f2ec74af69b842fa73e434
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-25 14:46:08 +0200

    set routingKey to empty

--- WebServer/src/RPCClient.java
+++ WebServer/src/RPCClient.java
@@ -9,55 +9,63 @@ import java.util.concurrent.TimeoutException;
 public class RPCClient {
 
 	private Connection connection;
-    private Channel channel;
-    private static final String EXCHANGE_NAME = "nameko-rpc";
-    private String routingKey = "kstream.stream";
-    private String replyQueueName;
+	private Channel channel;
+	private static final String EXCHANGE_NAME = "nameko-rpc";
+	private String routingKey = "";
+	private String replyQueueName;
 
-    public RPCClient() throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+	public RPCClient() throws IOException, TimeoutException {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
 
-        connection = factory.newConnection();
-        channel = connection.createChannel();
-        channel.exchangeDeclare(EXCHANGE_NAME, "topic", true);
-        replyQueueName = channel.queueDeclare().getQueue();
+		connection = factory.newConnection();
+		channel = connection.createChannel();
+		channel.exchangeDeclare(EXCHANGE_NAME, "topic", true);
+		replyQueueName = channel.queueDeclare().getQueue();
 
-        channel.queueBind(replyQueueName, EXCHANGE_NAME, replyQueueName);        
+		channel.queueBind(replyQueueName, EXCHANGE_NAME, replyQueueName);        
 
-    }
+	}
 
-    public String call(String message) throws IOException, TimeoutException, InterruptedException {
-        final String corrId = UUID.randomUUID().toString();
+	public String call(String message) throws IOException, TimeoutException, InterruptedException {
+		final String corrId = UUID.randomUUID().toString();
 
-        AMQP.BasicProperties props = new AMQP.BasicProperties
-                .Builder()
-                .correlationId(corrId)
-                .contentType("application/json")
-                .contentEncoding("utf-8")
-                .replyTo(replyQueueName)
-                .build();
+		AMQP.BasicProperties props = new AMQP.BasicProperties
+				.Builder()
+				.correlationId(corrId)
+				.contentType("application/json")
+				.contentEncoding("utf-8")
+				.replyTo(replyQueueName)
+				.build();
 
-        //System.out.println("Triple sent to the queue!");
-        channel.basicPublish(EXCHANGE_NAME, routingKey, props, message.getBytes("UTF-8"));
-        
+		//System.out.println("Triple sent to the queue!");
+		channel.basicPublish(EXCHANGE_NAME, routingKey, props, message.getBytes("UTF-8"));
 
-        final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
 
-        //System.out.println("Processing...");
-        channel.basicConsume(replyQueueName, true, new DefaultConsumer(channel) {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                if (properties.getCorrelationId().equals(corrId)) {
-                    response.offer(new String(body, "UTF-8"));
-                }
-            }
-        });
+		final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
 
-        return response.take();
-    }
-    
-    public void close() throws IOException {
-        connection.close();
-    }
+		//System.out.println("Processing...");
+		channel.basicConsume(replyQueueName, true, new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+				if (properties.getCorrelationId().equals(corrId)) {
+					response.offer(new String(body, "UTF-8"));
+				}
+			}
+		});
+
+		return response.take();
+	}
+
+	public void close() throws IOException {
+		connection.close();
+	}
+
+	public String getRoutingKey() {
+		return routingKey;
+	}
+
+	public void setRoutingKey(String routingKey) {
+		this.routingKey = routingKey;
+	}
 }
