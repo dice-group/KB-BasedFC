df89da67b5d165fd981ebfb0df2cbf3185837844
Rakshit K R <myfriendrakshit@gmail.com>
2019-05-25 14:45:30 +0200

    Update User_Data_Servlet.java

--- WebServer/src/User_Data_Servlet.java
+++ WebServer/src/User_Data_Servlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONObject;
 
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Logger;
 
 /**
@@ -39,12 +40,6 @@ public class User_Data_Servlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		// PrintWriter out = response.getWriter();
-		// response.setContentType("application/json");
-		// response.setCharacterEncoding("UTF-8");
-		// out.print(true_false_value);
-		// out.flush();
 	}
 
 	/**
@@ -59,25 +54,33 @@ public class User_Data_Servlet extends HttpServlet {
 		BufferedReader reader = request.getReader();
 		ObjectMapper mapper = new ObjectMapper();
 		PrintWriter out = response.getWriter();
-		
+
 		JSONObject jObject = new JSONObject(reader.readLine());
-		
+
 		reader.close();
-		
+
 		LOGGER.info("Received http request " + jObject.toString() + " from front-end");
-		
+
 		Fact fact = mapper.readValue(jObject.toString(), Fact.class);
 
 		LOGGER.info("Extracted values " + fact.getTaskId() + "," + fact.getSubject() + "," + fact.getPredicate() + "," + fact.getObject() + "," + fact.getAlgorithm());
-		
+
 		MessageForm message = new MessageForm();
-		
+
 		LOGGER.info("Sub, Pred, Obj sent to API component");
-		
-		message.sendData(fact);
-		
+
+		try {
+			message.sendData(fact);
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		LOGGER.info("Extracted truth score " + fact.getTruthValue() + " from the result");
-		
+
 		response.setContentType("application/json");
 		out.print(mapper.writeValueAsString(fact));
 		out.close();
